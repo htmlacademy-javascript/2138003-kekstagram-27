7a8af5fc412806dd5c19a7ea2e577394e352ea64
Sender60 <butkov60@gmail.com>
2022-10-02 19:02:25 +0300

    Добавление функции случайных чисел

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
 function getRandomNumber(min, max){
-  if(min < 0 || max < 0){
+  if(min < 0 || max < 0 || max <= min){
     return NaN;
   }
-  if(max <= min){
-    return NaN;
-  }
-  if(min <= max){
-    return Math.floor(min + Math.random() * (max + 1 - min));
-  }
+
+  return Math.floor(min + Math.random() * (max + 1 - min));
+
 }
 getRandomNumber(0,140);
