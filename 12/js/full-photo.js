f675f683b95a21ea66874410d9fab58f6d12badb
Keks <keks@htmlacademy.ru>
2022-11-22 20:13:56 +0300

    :heavy_check_mark: Сборка #12

--- 12/js/full-photo.js
+++ 12/js/full-photo.js
@@ -1,7 +1,8 @@
 import { isEscapeKey } from './util.js';
 
-
 const ONE_STEP = 5;
+let uploadMaximumComment = ONE_STEP;
+let buttonMaximumComment = ONE_STEP;
 const START_CYCLE_FOR_COMMENT = 0;
 const body = document.querySelector('body');
 const bigPicture = document.querySelector('.big-picture');
@@ -15,22 +16,16 @@ const commentCount = bigPicture.querySelector('.social__comment-count');
 const buttonLoader = bigPicture.querySelector('.comments-loader');
 const spanComment = commentCount.querySelector('.comments-visible');
 const cancelButtonBigPhoto = bigPicture.querySelector('.big-picture__cancel');
-const numberCurrentComment = parseInt(spanComment.textContent, Number);
-const commentListFragment = document.createDocumentFragment();
 
-let buttonMaximumComment = 5;
-
-const hidden = () => {
+const hiddenUploadComment = () => {
   if(parseInt(bigComments.firstChild.textContent, Number) <= parseInt(spanComment.textContent, Number) ){
     spanComment.textContent = bigComments.lastChild.textContent;
     buttonLoader.classList.add('hidden');
-  } else {
-    spanComment.textContent = 5;
-    buttonLoader.classList.remove('hidden');
   }
 };
 
 const createComment = (nameComment, startIndex, endIndex) => {
+  const commentListFragment = document.createDocumentFragment();
   for(let i = startIndex; i < endIndex; i++){
     const newComment = liComments.cloneNode(true);
     const imgComments = newComment.querySelector('.social__picture');
@@ -41,6 +36,7 @@ const createComment = (nameComment, startIndex, endIndex) => {
     textComments.textContent = nameComment.comments[i].message;
     commentListFragment.append(newComment);
   }
+  return commentListFragment;
 };
 
 const renderBigPhoto = (picture) =>{
@@ -53,42 +49,42 @@ const renderBigPhoto = (picture) =>{
   bigComments.textContent = picture.comments.length;
   bigDescription.textContent = picture.description;
 
-  hidden();
+  hiddenUploadComment();
 
-  const xx = () => {
-    let initialMaximumComment = 5;
-    if(bigComments.textContent < ONE_STEP){
-      initialMaximumComment = bigComments.textContent;
-    }
-    createComment(picture, START_CYCLE_FOR_COMMENT, initialMaximumComment);
-  };
-  xx();
-  selectorComments.innerHTML = '';
-  selectorComments.append(commentListFragment);
+  if(bigComments.textContent < ONE_STEP){
+    uploadMaximumComment = bigComments.textContent;
+  }
+
+  selectorComments.append(createComment(picture, START_CYCLE_FOR_COMMENT, uploadMaximumComment));
 
   const onClickUploadCommnent = (evt) => {
     evt.preventDefault();
+    const numberCurrentComment = parseInt(spanComment.textContent, Number);
     if(bigComments.textContent - spanComment.textContent < ONE_STEP){
       buttonMaximumComment = bigComments.textContent - spanComment.textContent;
     }
 
-    createComment(picture, numberCurrentComment, numberCurrentComment + buttonMaximumComment);
+    selectorComments.append(createComment(picture, numberCurrentComment, numberCurrentComment + buttonMaximumComment));
     spanComment.textContent = numberCurrentComment + ONE_STEP;
-    selectorComments.append(commentListFragment);
-    hidden();
+    hiddenUploadComment();
   };
   buttonLoader.addEventListener('click', onClickUploadCommnent);
-  cancelButtonBigPhoto.addEventListener('click', () => {
+
+  const closeBigPhoto = () => {
     bigPicture.classList.add('hidden');
     body.classList.remove('modal-open');
-    buttonMaximumComment = 5;
+    buttonMaximumComment = ONE_STEP;
+    uploadMaximumComment = ONE_STEP;
+    buttonLoader.removeEventListener('click', onClickUploadCommnent);
+    spanComment.textContent = ONE_STEP;
+    buttonLoader.classList.remove('hidden');
+  };
+  cancelButtonBigPhoto.addEventListener('click', () => {
+    closeBigPhoto();
   });
-
   document.addEventListener('keydown', (evt) => {
     if (isEscapeKey(evt)) {
-      bigPicture.classList.add('hidden');
-      body.classList.remove('modal-open');
-      buttonMaximumComment = 5;
+      closeBigPhoto();
     }
   });
 };
